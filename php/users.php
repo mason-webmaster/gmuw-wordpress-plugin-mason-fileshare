bb8bfe47ba669ddd77d304787bf3b15d23918235
Jan Macario <jmacario@gmu.edu>
2024-09-28 14:43:34 -0400

    Adds feature to redirect subscriber type users to the WP dashboard on login, rather than to their user profile page, as they would be by default.

--- php/users.php
+++ php/users.php
@@ -4,6 +4,27 @@
  * Summary: php file which implements customizations related to users
  */
 
+/**
+ * WordPress function for redirecting users on login based on user role
+ */
+add_filter( 'login_redirect', 'gmuw_fs_login_redirect_subscribers', 10, 3 );
+function gmuw_fs_login_redirect_subscribers( $url, $request, $user ) {
+
+	//if user is a subscriber, redirect them to the WP dashboard (instead of their user profile page, which they would be by default)
+	if (
+		//$user &&
+		//is_object($user) &&
+		is_a($user, 'WP_User') &&
+		$user->has_cap('subscriber')
+	) {
+		$url = '/wp-admin/index.php';
+	}
+
+	//return value
+	return $url;
+
+}
+
 //action to take when new users are created
 add_action( 'user_register', 'gmuw_fs_new_user', 10, 1 );
 function gmuw_fs_new_user( $user_id ) {

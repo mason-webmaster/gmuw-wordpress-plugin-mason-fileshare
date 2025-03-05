a1889e4f11100f2d1b338e10f85d02d32e796f97
Jan Macario <jmacario@gmu.edu>
2025-03-05 17:53:33 -0500

    Instead of using the WordPress default 'subscriber' role for our regular users and giving this role increased permissions, we create a custom mason_user role for this purpose.

--- php/permissions.php
+++ php/permissions.php
@@ -7,23 +7,32 @@
 
 function gmuw_fs_roles_and_caps_setup() {
 
-	//add upload_files capability to subscriber role
-	get_role('subscriber')->add_cap('upload_files');
+    // set up mason_user role. this role is intended for regular Mason users of this system
+
+    // first, remove the role if it exists
+    remove_role('mason_user');
+
+    // then re-add the role
+    add_role('mason_user','Mason User',array());
+
+    // add the read and upload_files capability to the role
+	get_role('mason_user')->add_cap('read');
+	get_role('mason_user')->add_cap('upload_files');
 
 }
 
 function gmuw_fs_roles_and_caps_cleanup() {
 
-	//remove upload_files capability from subscriber role
-	get_role('subscriber')->remove_cap('upload_files');
+    // portal_user
+    remove_role('mason_user');
 
 }
 
 /**
- * gmuw_fs_allow_edit_posts_for_subscribers_for_attachments()
+ * gmuw_fs_allow_edit_posts_for_mason_users_for_attachments()
  *
  * Filter on the current_user_can() function.
- * This function is used to explicitly subscribers to have the capablity to edit their own attachment posts, without giving them the edit_posts capablity generally (which would give them more than we want). This enables them to make use of the enable media replace plugin on the attachment edit page for their own uploaded files.
+ * This function is used to explicitly allow mason_users to have the capablity to edit their own attachment posts, without giving them the edit_posts capablity generally (which would give them more permissions than we want). This enables them to make use of the enable media replace plugin on the attachment edit page for their own uploaded files.
  *
  * @param array $allcaps All the capabilities of the user
  * @param array $cap     [0] Required capability
@@ -31,19 +40,19 @@ function gmuw_fs_roles_and_caps_cleanup() {
  *                       [1] User ID
  *                       [2] Associated object ID
  */
-add_filter( 'user_has_cap', 'gmuw_fs_allow_edit_posts_for_subscribers_for_attachments', 10, 3 );
-function gmuw_fs_allow_edit_posts_for_subscribers_for_attachments( $allcaps, $cap, $args ) {
+add_filter( 'user_has_cap', 'gmuw_fs_allow_edit_posts_for_mason_users_for_attachments', 10, 3 );
+function gmuw_fs_allow_edit_posts_for_mason_users_for_attachments( $allcaps, $cap, $args ) {
 
 	//bail out if we're not asking about editing a post
 	if ('edit_post' != $args[0])
 		return $allcaps;
 
-	//bail out if user isn't a subscriber (have subscriber capability)
-	if (!isset($allcaps['subscriber']))
+	//bail out if user isn't a mason_user (have mason_user capability)
+	if (!isset($allcaps['mason_user']))
 		return $allcaps;
 
-	//bail out if subscriber capability is false
-	if ($allcaps['subscriber']!=1)
+	//bail out if mason_user capability is false
+	if ($allcaps['mason_user']!=1)
 		return $allcaps;
 
 	//load the post data
@@ -57,7 +66,7 @@ function gmuw_fs_allow_edit_posts_for_subscribers_for_attachments( $allcaps, $ca
 	if ($mypost->post_type!='attachment')
 		return $allcaps;
 
-	//if we're still here, this user is a subscriber, and the system is asking whether they have the capability to edit a particular attachment post
+	//if we're still here, this user is a mason_user, and the system is asking whether they have the capability to edit a particular attachment post
 	//add the edit_posts capability
 	$allcaps['edit_posts']=1;
 
